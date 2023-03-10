b200519828a413bdbd351735dcbbc091804355e4
IHateMyKite <globglogabgalab991@gmail.com>
2023-03-10 18:15:08 +0100

    Updated action and batch script

--- PAPYRUS/BUILD.bat
+++ PAPYRUS/BUILD.bat
@@ -1,1 +1,1 @@
-PapyrusCompiler "../../Scripts/Source" -o="build_res" -all -i="../../Scripts/Source;SRC;SRC_OTHER;SRC_DD;SRC_SKYUI;SRC_UIEXT;SRC_PAPUTIL;SRC_SXL;SRC_MFG;SRC_RM;SRC_FNIS;SRC_SLA;SRC_IWW;SRC_XPMSE;SRC_ST" -optimize -f="Flags.flg"
+PapyrusCompiler "../Scripts/Source" -o="build_res" -all -i="../Scripts/Source;SRC;SRC_OTHER;SRC_DD;SRC_SKYUI;SRC_UIEXT;SRC_PAPUTIL;SRC_SXL;SRC_MFG;SRC_RM;SRC_FNIS;SRC_SLA;SRC_IWW;SRC_XPMSE;SRC_ST" -optimize -f="Flags.flg"
